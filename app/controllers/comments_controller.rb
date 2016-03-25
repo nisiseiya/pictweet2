2c3228cccae8e700058879173c055055a7d5010f
西聖勇弥 <seiya1@nisiseiya.local>
2016-03-25 17:02:07 +0900

    permit_add_to

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(text: params[:text], tweet_id: params[:tweet_id], user_id: current_user.id)
+  end
+
+  private
+  def comment_params
+    params.permit(:text, :tweet_id)
+  end
 end
